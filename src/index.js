873b1acb302607771ed745f424594fc85037d09e
SamadhanBhutekar <samadhanbhutekar02@gmail.com>
2025-09-05 15:33:41 +0530

    WD

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const pool = require("./db");
+const pool = require("../config/db_api");
 
 const app = express();
 app.use(cors());
