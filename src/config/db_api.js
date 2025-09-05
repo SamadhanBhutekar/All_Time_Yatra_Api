ffacd8aa8ed11c208c4bdab4d9bf609d16134729
SamadhanBhutekar <samadhanbhutekar02@gmail.com>
2025-09-05 19:04:17 +0530

    WD

--- src/config/db_api.js
+++ src/config/db_api.js
@@ -1,14 +1,17 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST, // public Railway host
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306,
 });
 
-module.exports = pool;
+connection.connect((err) => {
+  if (err) {
+    console.error("DB connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database!");
+  }
+});
