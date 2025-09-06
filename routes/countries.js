b08dda29c715135aa70514a368546ff942b734cc
SamadhanBhutekar <samadhanbhutekar02@gmail.com>
2025-09-06 17:20:26 +0530

    WD

--- routes/countries.js
+++ routes/countries.js
@@ -1,9 +1,7 @@
-// routes/countries.js
 const express = require("express");
 const router = express.Router();
 const countriesController = require("../controllers/countriesController");
 
-// POST /api/countries
-router.post("/countries", countriesController.getAllCountries);
+router.post("/oneway_country", countriesController.getAllCountries);
 
 module.exports = router;
