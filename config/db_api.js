ede253389286a159a338229fc15180e45d41aa53
SamadhanBhutekar <samadhanbhutekar02@gmail.com>
2025-09-06 17:14:51 +0530

    DB

--- config/db_api.js
+++ config/db_api.js
@@ -2,8 +2,9 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS, // ✅ fixed
+  password: process.env.DB_PASSWORD, // ✅ correct
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
