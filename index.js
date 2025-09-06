e4f9e8eb6276aaba71877965d9f9e237b7ed6f6d
SamadhanBhutekar <samadhanbhutekar02@gmail.com>
2025-09-06 11:20:45 +0530

    WD

--- index.js
+++ index.js
@@ -11,41 +11,7 @@ const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
-//
-// app.use(cors());
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const session = require("express-session");
-
-app.use(
-  session({
-    secret: "jeeyoride@admin", // use strong key in production
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }, // secure: true only when using HTTPS
-  })
-);
-
-// ✅ Correctly placed CORS config (FIRST middleware)
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "PATCH"],
-    credentials: true,
-    allowedHeaders: ["Content-Type"],
-  })
-);
-app.use(express.json());
-//
-//
-// Static files (for accessing uploaded images)
-app.use("/", express.static(path.join(process.cwd(), "uploads")));
-
-app.use("drivers_kyc", express.static(__dirname + "drivers_kyc"));
-
 app.get("/", (req, res) => res.send({ message: "Server is alive!" }));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
